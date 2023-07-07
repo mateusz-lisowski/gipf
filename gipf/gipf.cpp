b93ad0924df06f7dfc37892b2727bf48851c81bc
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-07-07 15:30:30 +0200

    Added Game and Player structures with basic methods

--- gipf/gipf.cpp
+++ gipf/gipf.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+struct Player {
+
+
+
+};
+
+
+struct Game {
+
+    Player* current_player;
+
+    int size;
+    int pieces_limit;
+    int white_player_pieces;
+    int black_player_pieces;
+
+    Game() {
+        scan_config(this);
+    }
+
+    void scan_config(Game* game) {
+        
+    
+    }
+
+};
+
+
+int main() {
+
+    Game game;
+
+    game.scan_config()
+
+    Player white;
+    Player black;
+
 }
