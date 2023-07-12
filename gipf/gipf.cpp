68afe94ab6cd1e3f7c34175decabf5e249bf192d
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-07-12 15:28:21 +0200

    Second test passed.

--- gipf/gipf.cpp
+++ gipf/gipf.cpp
@@ -9,11 +9,24 @@
 #define WHITE 'W'
 #define BLACK 'B'
 
+#define DIRECTION_HOR 0
+#define DIRECTION_VERT 1
+#define DIRECTION_DIAGONAL 2
+
 
 struct Collision {
 
-    int begin;
-    int end;
+    char* begin;
+    char* end;
+    int direction;
+};
+
+struct Move {
+
+    int x;
+    int y;
+    int direction;
+    bool positive;
 
 };
 
@@ -36,6 +49,8 @@ struct Game {
     int black_player_pieces;
 
     Board board;
+    std::vector<Move> moves;
+
 
     void create_empty_board() {
 
@@ -48,14 +63,37 @@ struct Game {
             int max_y = 2 * size - 1 - offset;
 
             for (int j = 1; j <= max_y; j++) {
-                *calculate_index(i, j) = EMPTY;
+                *get_char_at_index(board, i, j) = EMPTY;
             }
+        }
+
+        generate_all_moves();
+    }
+
+    int calculate_direction(int dir) {
+
+        if (dir == DIRECTION_HOR) {
+            return 1;
+        }
+        else if (dir == DIRECTION_VERT) {
+            return 2 * size + 1;
+        }
+        else {
+            return 2 * size + 2;
         }
     }
 
-    char* calculate_index(int x, int y) {
+    char* get_char_at_index(Board& b, int x, int y) {
         
         int i;
+
+        if (x > 2 * size) {
+            return nullptr;
+        }
+
+        if (y > 2 * size - abs(size - x)) {
+            return nullptr;
+        }
 
         if (x <= size) {
             i = (2 * size + 1) * y + x;
@@ -65,7 +103,7 @@ struct Game {
             i = (2 * size + 1) * (y + x - size) + x;
         }
 
-        return &board.board[i];
+        return &b.board[i];
     }
 
     void load_game_board() {
@@ -75,17 +113,30 @@ struct Game {
 
         create_empty_board();
 
+        int dir_dig = calculate_direction(DIRECTION_DIAGONAL);
+
         std::string line;
         std::getline(std::cin, line);
 
-        auto it = board.board.begin();
-        auto end = board.board.end();
-
         for (int i = 0; i < size * 2 - 1; i++) {
+
+            int x, y;
+
+            if (i < size) {
+                x = 1;
+                y = size - i;
+            }
+            else
+            {
+                x = i - size + 2;
+                y = 1;
+            }
 
             int offset = abs(size - i - 1);
 
             std::getline(std::cin, line);
+            while (line.back() == ' ')
+                line.pop_back();
 
             if (offset + 2 * (size * 2 - 1 - offset) - 1 != line.size()) {
                 std::cout << "WRONG_BOARD_ROW_LENGTH\n";
@@ -93,14 +144,14 @@ struct Game {
                 return;
             }
 
+            char* board_ch = get_char_at_index(board, x, y);
+
             for (int j = 0; j < size * 2 - 1 - offset; j++) {
 
                 char ch = line[offset + 2 * j];
 
-                while (it != end && *it == NULL)
-                    it++;
-
-                *it++ = ch;
+                *board_ch = ch;
+                board_ch += dir_dig;
 
             }
         }
@@ -124,6 +175,8 @@ struct Game {
 
     void print_game_board() {
     
+        int dir_dig = calculate_direction(DIRECTION_DIAGONAL);
+
         if (board.board.empty()) {
             
             std::cout << "EMPTY_BOARD\n";
@@ -138,28 +191,232 @@ struct Game {
 
         for (int i = 0; i < size * 2 - 1; i++) {
 
+            int x, y;
+
+            if (i < size) {
+                x = 1;
+                y = size - i;
+            }
+            else
+            {
+                x = i - size + 2;
+                y = 1;
+            }
+
             int offset = abs(size - i - 1);
             std::cout << std::string(offset, ' ');
 
+            char* board_ch = get_char_at_index(board, x, y);
+
             for (int j = 0; j < size * 2 - 1 - offset; j++) {
 
-                while (it != end && *it == NULL)
-                    it++;
-
-                char ch = *it++;
+                char ch = *board_ch;
+                board_ch += dir_dig;
 
                 std::cout << ch << " ";
             }
-
+                        
             std::cout << "\n";
         }
     
         std::cout << "\n";
     }
 
+    void advance_turn(Board& b) {
+
+        if (current_player == WHITE) {
+            current_player = BLACK;
+            b.white_reserve--;
+        }
+
+        else if (current_player == BLACK) {
+            current_player = WHITE;
+            b.black_reserve--;
+        }
+    }
+
+    void generate_all_moves() {
+        
+        moves.clear();
+
+        int dir_hor = calculate_direction(DIRECTION_HOR);
+        int dir_ver = calculate_direction(DIRECTION_VERT);
+        int dir_dig = calculate_direction(DIRECTION_DIAGONAL);
+
+        for (int i = 1; i < 2 * size; i++) {
+            for (int j = 1; j < 2 * size; j++) {
+
+                char* ch = get_char_at_index(board,i, j);
+
+                if (ch == nullptr || *ch == NULL) {
+                    continue;
+                }
+
+                if (*(ch + dir_hor) == NULL) moves.push_back(Move{ i, j, -dir_hor, false });
+                if (*(ch - dir_hor) == NULL) moves.push_back(Move{ i, j, +dir_hor, true });
+
+                if (*(ch + dir_ver) == NULL) moves.push_back(Move{ i, j, -dir_ver, false });
+                if (*(ch - dir_ver) == NULL) moves.push_back(Move{ i, j, +dir_ver, true });
+
+                if (*(ch + dir_dig) == NULL) moves.push_back(Move{ i, j, -dir_dig, false });
+                if (*(ch - dir_dig) == NULL) moves.push_back(Move{ i, j, +dir_dig, true });
+            }
+        }
+
+    
+    }
+
+    bool move(char* from, int direction) {
+        
+        char token = current_player;
+
+        while (true) {
+            
+            if (*from == NULL) {
+                return false;
+            }
+
+            char curr = *from;
+            *from = token;
+
+            if (curr == EMPTY) {
+                return true;
+            }
+            
+            token = curr;
+            from += direction;
+        }
+
+    }
+
+    std::vector<Collision> generate_all_collisions(Board& b) {
+        
+        std::vector<Collision> collisions;
+
+        for (auto& move : moves) {
+
+            if (!move.positive) {
+                continue;
+            }
+
+            char* ch = get_char_at_index(b, move.x, move.y);
+
+            char token = *ch;
+            int count = 1;
+
+            while (*ch != NULL) {
+                ch += move.direction;
+                char curr = *ch;
+
+                if (curr == token) {
+                    count++;
+                }
+                else
+                {
+                    if (count >= pieces_limit) {
+
+                        char* from = ch - count * move.direction;
+                        char* to = ch - move.direction;
+
+                        collisions.push_back(Collision{ from, to, move.direction });
+                    }
+
+                    token = curr;
+                    count = 1;
+                }
+            }
+
+        }
+
+        return collisions;
+    }
+
+    void remove_collision(Board& b, Collision c) {
+        
+        char* ch = c.begin;
+        char token = *ch;
+
+        while (*ch != EMPTY && *ch != NULL) {
+            ch -= c.direction;
+        }
+
+        ch += c.direction;
+
+        while (*ch != EMPTY && *ch != NULL) {
+            
+            char curr = *ch;
+
+            if (curr == current_player) {
+                
+                if (current_player == WHITE) {
+                    b.white_reserve++;
+                }
+                else if (current_player == BLACK) {
+                    b.black_reserve++;
+                }
+
+            }
+               
+            *ch = EMPTY;
+
+            ch += c.direction;
+        }
+    }
+
     void do_move(const std::string& line) {
         
+        int from_x = line[0] - 'a';
+        int from_y = line[1] - '1';
 
+        int to_x = line[3] - 'a';
+        int to_y = line[4] - '1';
+
+        char* from = get_char_at_index(board, from_x, from_y);
+        char* to = get_char_at_index(board, to_x, to_y);
+
+        if (from == nullptr) {
+            std::cout << "BAD_MOVE_" << std::string(line.begin() + 0, line.begin() + 2) << "_IS_WRONG_INDEX\n";
+            return;
+        }
+        if (to == nullptr) {
+            std::cout << "BAD_MOVE_" << std::string(line.begin() + 3, line.begin() + 5) << "_IS_WRONG_INDEX\n";
+            return;
+        }
+
+        if (*from != NULL) {
+            std::cout << "BAD_MOVE_" << std::string(line.begin() + 0, line.begin() + 2) << "_IS_WRONG_STARTING_FIELD\n";
+            return;
+        }
+        if (*to == NULL) {
+            std::cout << "BAD_MOVE_" << std::string(line.begin() + 3, line.begin() + 5) << "_IS_WRONG_DESTINATION_FIELD\n";
+            return;
+        }
+
+        int direction = to - from;
+
+        int dir_hor = calculate_direction(DIRECTION_HOR);
+        int dir_ver = calculate_direction(DIRECTION_VERT);
+        int dir_dig = calculate_direction(DIRECTION_DIAGONAL);
+
+        if (direction != dir_hor && direction != -dir_hor &&
+            direction != dir_ver && direction != -dir_ver &&
+            direction != dir_dig && direction != -dir_dig) {
+
+            std::cout << "UNKNOWN_MOVE_DIRECTION\n";
+            return;
+        }
+
+        from = to;
+        auto copy = board;
+
+        if (move(from, direction)) {
+            std::cout << "MOVE_COMMITTED\n";
+            advance_turn(board);
+        }
+        else {
+            board = copy;
+            std::cout << "BAD_MOVE_ROW_IS_FULL\n";
+        }
 
     }
 
@@ -175,12 +432,14 @@ int main() {
 
         if (line.rfind("LOAD_GAME_BOARD", 0) == 0) {
             game.load_game_board();
+            std::cout << "\n";
         }
         else if (line.rfind("PRINT_GAME_BOARD", 0) == 0) {
             game.print_game_board();
         }
         else if (line.rfind("DO_MOVE", 0) == 0) {
             game.do_move(std::string(line.begin() + 8, line.end()));
+            std::cout << "\n";
         }
 
     }
